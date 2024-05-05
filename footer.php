320a25bc411c82f77735958fa81e47bbdf27951f
Prem <Premm7655@gmail.com>
2024-05-05 23:23:12 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+	 <footer style="background-color: #4e4747;padding: 1vw";>
+    <p style="color: white; font-weight: bold;
+    font-size: 1vw;" class="pull-right"><a href="#">Back to top</a></p>
+    <p style="color: white;    font-weight: bold;font-size: 1vw;text-shadow: aqua;text-align: center;color: white;  " >You have reached the bottom of the page</p>
+
+  </div>
+		
+</footer>
+</body>
+</html>
